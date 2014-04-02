8b4eb88a55e7eb6d362cb744d9c0550b69df1b1b
Kevin <kevinferm@gmail.com>
2014-04-02 11:54:34 +0200

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zombies::Application.config.secret_key_base = 'a125b868baa948519ef1c4e06ba85e29466606dfb795ab04ec72c3ee976ee520cf6b834f76eca00c0609b4f2e9d335ed9784951a95cddd1d04a03666365e7fb5'
